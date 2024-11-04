7154451cb120f2db11f2158e29cfe9a5f155bea7
flobatard <fb.batard@gmail.com>
2024-11-04 11:35:09 +0100

    Test files

--- makefile
+++ makefile
@@ -11,12 +11,20 @@ INCLUDESDIR=includes$(SEP)
 OBJ=obj$(SEP)
 BIN=bin$(SEP)
 LIB=lib$(SEP)
+TEST=test$(SEP)
 DISTR=distr$(SEP)
+
 
 SOURCES=LMDB_wrapper.cpp
 OBJS=$(SOURCES:%.cpp=%.o)
 PATHOBJS=$(OBJS:%=$(OBJ)%)
 WARNINGS=-Wall -Wextra -pedantic
+
+TEST_SOURCES=concurrency_test.cpp simple_test.cpp
+TEST_BIN=$(TEST)bin$(SEP)
+TEST_OBJ=$(TEST)obj$(SEP)
+TEST_OBJS=$(TEST_SOURCES:$(TEST)$(SEP)%.cpp=$(TEST)$(SEP)%.o)
+TEST_EXES=$(TEST_SOURCES:%.cpp=test_%.exe)
 
 DEPENDENCIES=liblmdb.a
 
@@ -27,10 +35,19 @@ FLAGS= -std=c++23 $(WARNINGS) -I$(HEADERDIR) -I$(INCLUDESDIR) -L$(LIB)
 all: $(OBJS) main.o
 	$(CC) $(FLAGS) -o $(BIN)$(NAME).exe -static $(OBJ)main.o $(OBJS:%=$(OBJ)%) $(LIBS)
 
+tests: $(TEST_EXES)
+	@echo Test generated
+
+test_%.exe: test_%.o
+	$(CC) $(FLAGS) -o $(TEST_BIN)$(@:test_%.exe=%.exe) -static $(TEST_OBJ)$(@:test_%.exe=%.o) $(OBJS:%=$(OBJ)%) $(LIBS)
+
+test_%.o:
+	$(CC) -g $(FLAGS) -c $(TEST)$(@:test_%.o=%.cpp) -o $(TEST)$(SEP)$(OBJ)$(@:test_%.o=%.o) -static
+
 main.o:
 	$(CC) -g $(FLAGS) -c main.cpp -o $(OBJ)main.o -static
 
-%.o: 
+%.o:
 	$(CC) -g $(FLAGS) -c $(SOURCEDIR)$(@:%.o=%.cpp) -o $(OBJ)$@ -static
 
 share: $(OBJS)
---
+++ test/simple_test.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<vector>
+#include<chrono>
+#include"LMDB_wrapper.h"
+
+#ifdef _WIN32
+    #define SEP "\\"
+#else
+    #define SEP "/"
+#endif
+
+using namespace std::chrono;
+using namespace std;
+
+int mass_put(LMDBWrapper& lmdb, int offset, int count = 1000)
+{
+    for (int i = offset; i < count + offset; i++)
+    {
+        lmdb.put("toto_" + std::to_string(i), "tata_" + std::to_string(i));
+    }
+    for (int i = offset; i < count + offset; i++)
+    {
+        std::string value = lmdb.get("toto_" + std::to_string(i));
+        if (!(value == "tata_" + std::to_string(i)))
+        {
+            std::cerr << "Error value is not the same" << std::endl; 
+            throw "Error value is not the same: DB= " + value + " test=" + "tata_" + std::to_string(i);
+        }
+    }
+    return 0;
+}
+
+int mass_delete(LMDBWrapper& lmdb, int offset, int count = 1000)
+{
+    for (int i = offset; i < count + offset; i++)
+    {
+        lmdb.remove("toto_" + std::to_string(i));
+    }
+    for (int i = offset; i < count + offset; i++)
+    {
+        std::string value = lmdb.get("toto_" + std::to_string(i));
+        if (!(value == ""))
+        {
+            std::cerr << "Error value is not the same" << std::endl; 
+            throw "Error value is not the same: DB= " + value + " test=" + "tata_" + std::to_string(i);
+        }
+    }
+    return 0;
+}
+
+int mass_get(LMDBWrapper& lmdb, int offset, int count = 1000)
+{
+    for (int i = offset; i < count + offset; i++)
+    {
+        std::string value = lmdb.get("toto_" + std::to_string(i));
+        if (!(value == "tata_" + std::to_string(i)))
+        {
+            std::cerr << "Error value is not the same" << std::endl;
+            throw "Error value is not the same: DB= " + value + " test=" + "tata_" + std::to_string(i);
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    std::cout << "__cplusplus: " << __cplusplus;
+    std::cout << std::endl;
+    LMDBWrapper lmdb = LMDBWrapper("db1");
+    int nb_chunks = 10;
+    int chunk_size = 1000;
+
+    auto start = high_resolution_clock::now();
+
+    for (int i = 0; i < nb_chunks; i++)
+    {
+        mass_put(lmdb, i*chunk_size, chunk_size);
+    }
+    auto putClock = high_resolution_clock::now();
+    
+    auto duration = duration_cast<microseconds>(putClock - start);
+    std::cout << "Put chrono " << duration.count() << "micros" << endl;
+
+    for (int i = 0; i < nb_chunks; i++)
+    {
+        mass_get(lmdb, i*chunk_size, chunk_size);
+    }
+    auto getClock = high_resolution_clock::now();
+    
+    auto duration3 = duration_cast<microseconds>(getClock - putClock);
+    std::cout << "Get chrono " << duration3.count() << "micros" << endl;
+    
+
+    for (int i = 0; i<nb_chunks; i++)
+    {
+        mass_delete(lmdb, i*chunk_size, chunk_size);
+    }
+
+    auto endClock = high_resolution_clock::now();
+    auto duration2 = duration_cast<microseconds>(endClock - putClock);
+    std::cout << "Delete chrono " << duration2.count() << "micros" << endl;
+    return 0;
+}
